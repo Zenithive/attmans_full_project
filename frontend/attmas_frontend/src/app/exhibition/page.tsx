6f73fb65c17c5b945d1fc6a52d9d17de07fe1729
pratikp_72 <pratikp72@hotmail.com>
2024-05-30 00:36:42 +0530

    Add exhibition component

--- frontend/attmas_frontend/src/app/exhibition/page.tsx
+++ frontend/attmas_frontend/src/app/exhibition/page.tsx
@@ -1,12 +1,13 @@
 import { Box, Button, colors } from '@mui/material'
 import React from 'react'
+import { AddExhibition } from '../component/exhibition/add-exhibition'
 
 const Exhibition = () => {
   return (
     <Box sx={{borderRadius: 3, background: colors.grey}}>
       <Box sx={{display: 'flex' , justifyContent: 'space-between'}}>
         <Box component="h2" sx={{marginY: 0}}>Exhibitions</Box>
-        <Button type='button' size='small' variant='contained' sx={{borderRadius: 3}}>Create Exhibition</Button>
+        <AddExhibition />
       </Box>
     </Box>
   )
