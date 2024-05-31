9a388504163f7394a8cb7ddb2a6a5ef2f8849756
your username <sachint@zenithive.com>
2024-05-31 16:03:12 +0530

    Your branch is up to date with 'origin/sachin_dev_branch'.
     Changes to be committed:
    	modified:   frontend/attmas_frontend/src/app/profile/page.tsx

--- frontend/attmas_frontend/src/app/profile/page.tsx
+++ frontend/attmas_frontend/src/app/profile/page.tsx
@@ -1,26 +1,3 @@
-// "use client"
-// import React from 'react'
-// import Profile1 from '../component/all_Profile_component/profile1'
-// import Profile2 from '../component/all_Profile_component/profile2'
-// import Profile3 from '../component/all_Profile_component/profile3'
-// import HorizontalStepper from '../component/all_Profile_component/upperLine'
-// import Category from '../component/all_Profile_component/multipleDropDownCategory'
-
-// const page = () => {
-//   return (
-//     <>
-//     <HorizontalStepper/>
-//     <Profile1/>
-//     <Profile2/>
-//     <Profile3/>
-//     <Category/>
-//     </>
-//   )
-// }
-
-// export default page
-
-
 "use client";
 import React, { useState } from 'react';
 import Profile1 from '../component/all_Profile_component/profile1';
@@ -47,8 +24,12 @@ const Page = () => {
       {currentStep === 2 && (
         <Profile2 onNext={handleNext} onPrevious={handlePrevious} />
       )}
-      {currentStep === 3 && <Profile3 onPrevious={handlePrevious} />}
-      <Category />
+      {currentStep === 3 && (
+        <>
+          <Profile3 onPrevious={handlePrevious} />
+          <Category />
+        </>
+      )}
     </>
   );
 };
