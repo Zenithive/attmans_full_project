fa68dcf08d02a9ded73b67099d55d4b3c464692b
your username <sachint@zenithive.com>
2024-06-20 17:57:41 +0530

    All done producrion build ready

--- frontend/attmas_frontend/src/app/component/ProfileSeprateComponent/ProfileFormFields.tsx
+++ frontend/attmas_frontend/src/app/component/ProfileSeprateComponent/ProfileFormFields.tsx
@@ -1,10 +1,14 @@
-"use client"
+'use client'
 import React from 'react';
 import { Grid, TextField, MenuItem } from '@mui/material';
 import { useFormikContext } from 'formik';
 
 const ProfileFormFields: React.FC = () => {
   const { values, handleChange, handleBlur, touched, errors } = useFormikContext<any>();
+
+  const getHelperText = (field: string) => {
+    return touched[field] && typeof errors[field] === 'string' ? errors[field] : undefined;
+  };
 
   return (
     <Grid container spacing={2}>
@@ -15,13 +19,13 @@ const ProfileFormFields: React.FC = () => {
           rows={6}
           id="address"
           label="Address"
-          color='secondary'
+          color="secondary"
           name="address"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.address}
           error={touched.address && Boolean(errors.address)}
-          helperText={touched.address && errors.address}
+          // helperText={getHelperText('address')}
           InputProps={{
             style: {
               borderRadius: '25px',
@@ -38,80 +42,80 @@ const ProfileFormFields: React.FC = () => {
       <Grid item xs={12} sm={6}>
         <TextField
           fullWidth
-          style={{ background: "white", borderRadius: "25px" }}
+          style={{ background: 'white', borderRadius: '25px' }}
           id="city"
           label="City"
-          color='secondary'
+          color="secondary"
           name="city"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.city}
           error={touched.city && Boolean(errors.city)}
-          helperText={touched.city && errors.city}
+          // helperText={getHelperText('city')}
         />
       </Grid>
 
       <Grid item xs={12} sm={6}>
         <TextField
           fullWidth
-          style={{ background: "white", borderRadius: "25px" }}
+          style={{ background: 'white', borderRadius: '25px' }}
           id="state"
           label="State"
-          color='secondary'
+          color="secondary"
           name="state"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.state}
           error={touched.state && Boolean(errors.state)}
-          helperText={touched.state && errors.state}
+          // helperText={getHelperText('state')}
         />
       </Grid>
 
       <Grid item xs={12} sm={6}>
         <TextField
           fullWidth
-          style={{ background: "white", borderRadius: "25px" }}
+          style={{ background: 'white', borderRadius: '25px' }}
           id="pinCode"
           label="Pin Code"
-          color='secondary'
+          color="secondary"
           name="pinCode"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.pinCode}
           error={touched.pinCode && Boolean(errors.pinCode)}
-          helperText={touched.pinCode && errors.pinCode}
+          // helperText={getHelperText('pinCode')}
         />
       </Grid>
 
       <Grid item xs={12} sm={6}>
         <TextField
           fullWidth
-          style={{ background: "white", borderRadius: "25px" }}
+          style={{ background: 'white', borderRadius: '25px' }}
           id="country"
           label="Country"
-          color='secondary'
+          color="secondary"
           name="country"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.country}
           error={touched.country && Boolean(errors.country)}
-          helperText={touched.country && errors.country}
+          // helperText={getHelperText('country')}
         />
       </Grid>
 
       <Grid item xs={12} sm={6}>
         <TextField
           fullWidth
-          style={{ background: "white", borderRadius: "25px" }}
+          style={{ background: 'white', borderRadius: '25px' }}
           id="linkedIn"
           label="LinkedIn"
-          color='secondary'
+          color="secondary"
           name="linkedIn"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.linkedIn}
           error={touched.linkedIn && Boolean(errors.linkedIn)}
-          helperText={touched.linkedIn && errors.linkedIn}
+          // helperText={getHelperText('linkedIn')}
         />
       </Grid>
 
@@ -119,16 +123,16 @@ const ProfileFormFields: React.FC = () => {
         <TextField
           fullWidth
           select
-          style={{ background: "white", borderRadius: "25px" }}
+          style={{ background: 'white', borderRadius: '25px' }}
           id="gender"
           label="Gender"
-          color='secondary'
+          color="secondary"
           name="gender"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.gender}
           error={touched.gender && Boolean(errors.gender)}
-          helperText={touched.gender && errors.gender}
+          // helperText={getHelperText('gender')}
         >
           <MenuItem value="Male">Male</MenuItem>
           <MenuItem value="Female">Female</MenuItem>
@@ -141,16 +145,16 @@ const ProfileFormFields: React.FC = () => {
           fullWidth
           multiline
           rows={6}
-          style={{ background: "white", borderRadius: "25px" }}
+          style={{ background: 'white', borderRadius: '25px' }}
           id="billingAddress"
           label="Billing Address"
-          color='secondary'
+          color="secondary"
           name="billingAddress"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.billingAddress}
           error={touched.billingAddress && Boolean(errors.billingAddress)}
-          helperText={touched.billingAddress && errors.billingAddress}
+          // helperText={getHelperText('billingAddress')}
           InputProps={{
             style: {
               borderRadius: '25px',
@@ -165,6 +169,6 @@ const ProfileFormFields: React.FC = () => {
       </Grid>
     </Grid>
   );
-}
+};
 
 export default ProfileFormFields;
