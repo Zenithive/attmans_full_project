f43b62ebe89e62535ae4439ee3b5f99fe1dfc1ad
Sachin Thakkar <sachint@zenithive.com>
2024-07-15 15:47:47 +0530

    Website done attmas

--- frontend/attmas_frontend/src/app/component/SignUp/signup.tsx
+++ frontend/attmas_frontend/src/app/component/SignUp/signup.tsx
@@ -24,7 +24,7 @@ function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="https://attmans.netlify.app/">
         Your Website
       </Link>{' '}
       {new Date().getFullYear()}
