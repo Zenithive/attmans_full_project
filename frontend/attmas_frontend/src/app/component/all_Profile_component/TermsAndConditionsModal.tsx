211c2b267287473229437be4b29b945a66848367
Sachin Thakkar <sachint@zenithive.com>
2024-08-30 01:24:25 +0530

    terms and condition

---
+++ frontend/attmas_frontend/src/app/component/all_Profile_component/TermsAndConditionsModal.tsx
@@ -0,0 +1,75 @@
+// components/TermsAndConditionsModal.tsx
+
+import React from 'react';
+import { Modal, Box, Typography, Button, List, ListItem, ListItemText } from '@mui/material';
+import { useAppSelector } from '@/app/reducers/hooks.redux';
+import { selectUserSession, UserSchema } from '@/app/reducers/userReducer';
+import { innovatorTerms, freelancerTerms, projectOwnerTerms } from './const';
+
+interface TermsAndConditionsModalProps {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const TermsAndConditionsModal: React.FC<TermsAndConditionsModalProps> = ({ open, onClose, onConfirm }) => {
+  const userDetails: UserSchema = useAppSelector(selectUserSession);
+
+  // Determine which terms to show based on the user type
+  const termsToShow = userDetails.userType === 'Innovators'
+    ? innovatorTerms
+    : userDetails.userType === 'Freelancer'
+    ? freelancerTerms
+    : userDetails.userType === 'Project Owner'
+    ? projectOwnerTerms
+    : 'Please read and accept the terms and conditions before proceeding.';
+
+  // Split terms text into sections based on numbering and bullet points
+  const splitTerms = termsToShow.split(/\d+\./).map(section => section.trim()).filter(section => section !== '');
+  
+  // Extract bullet-pointed items
+  const formattedTerms = splitTerms.flatMap((section, index) => {
+    const items = section.split(/●/).map(item => item.trim()).filter(item => item !== '');
+    return index === 0 ? items : items.map(item => `• ${item}`);
+  });
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="terms-conditions-title"
+      aria-describedby="terms-conditions-description"
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 800, // Increased width for a bigger modal
+          bgcolor: 'background.paper',
+          boxShadow: 24,
+          p: 4,
+          borderRadius: 2,
+          maxHeight: '80vh', // to handle large text by scrolling
+          overflowY: 'auto',
+        }}
+      >
+        <Typography id="terms-conditions-title" variant="h6" component="h2">
+          Terms and Conditions
+        </Typography>
+        <pre dangerouslySetInnerHTML={{ __html: termsToShow }}        ></pre>
+        <Box sx={{ mt: 3, display: 'flex', justifyContent: 'flex-end' }}>
+          <Button variant="outlined" onClick={onClose} sx={{ mr: 2 }}>
+            Cancel
+          </Button>
+          <Button variant="contained" onClick={onConfirm}>
+            Accept
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
+
+export default TermsAndConditionsModal;
---
+++ frontend/attmas_frontend/src/app/component/all_Profile_component/const.tsx
@@ -0,0 +1,209 @@
+export const innovatorTerms = `
+Terms and Conditions for Innovators
+Welcome! Thank you for your interest in our platform. These Terms and Conditions
+("Terms") govern your use of our platform as an innovator offering innovative and
+patented products ("Products") and research collaboration services ("Services"). By
+submitting a Product or Service listing, you agree to be bound by these Terms.
+<b>1. Eligibility</b>
+    ● You must be a legal entity or individual with the right to sell the Products and
+      provide the Services you offer.
+    ● You must be the legal owner of the patents associated with your Products.
+<b>2. Product and Service Listings</b>
+    ● You are responsible for the accuracy and completeness of all information you
+      provide about your Products and Services.
+    ● You agree to only list Products that are innovative or have a valid patent.
+    ● You agree to only list Services that you are qualified to provide.
+<b>3. Intellectual Property</b>
+    ● You retain ownership of all intellectual property rights associated with your
+      Products and Services.
+    ● By listing your Products or Services, you grant us a non-exclusive, royalty-free
+      license to display that information on our platform.
+<b>4. Transactions</b>
+    ● We are a platform to connect companies with innovators. We do not facilitate
+      transactions directly.
+
+    ● You are responsible for negotiating all terms of any agreements with companies,
+      including price, payment terms, and delivery.
+    ● We recommend that you consult with an attorney to create standard agreements
+      for your Products and Services.
+<b>5. Fees</b>
+
+    ● We may charge a fee for listing your Products or Services on our platform.
+    ● The specific fees will be clearly communicated before you submit a listing.
+<b>6. Term and Termination</b>
+    ● These Terms will remain in effect until terminated by either party.
+    ● We may terminate your access to the platform for any reason, with or without
+      notice.
+    ● You may terminate your use of the platform at any time.
+<b>7. Disclaimer</b>
+    ● We do not provide any warranty or guarantee regarding the success of your
+      listings or the quality of companies you connect with.
+<b>8. Limitation of Liability</b>
+    ● We will not be liable for any damages arising from your use of the platform or
+      your interactions with companies.
+<b>9. Governing Law and Dispute Resolution</b>
+If a dispute arises between you and ATTMANS, our goal is to resolve the dispute
+quickly and cost-effectively. Accordingly, you and ATTMANS agree to resolve any
+claim or controversy at law or equity that arise out of or relate to this Agreement or
+the Services (each, a "Claim") by referring the Claim to binding arbitration. The
+
+arbitration shall be conducted in Ahmedabad, Gujarat, India in accordance with the
+terms of the Indian Law Arbitration Rules which are incorporated herein by way of
+reference.
+Subject to arbitration, this Agreement including without limitation any dispute
+relating to the Services, shall be governed by and construed in accordance with
+Indian Laws, without regard to its conflict of law provisions and the Courts of
+Ahmedabad shall have the sole jurisdiction in this regard.
+<b>10. Entire Agreement</b>
+    ● These Terms constitute the entire agreement between you and us regarding your
+      use of the platform.
+<b>11. Updates to Terms</b>
+    ● The ATTMANS reserves the right to update these Terms at any time.
+    ● Continued use of the ATTMANS after the update constitutes your agreement to
+      the revised Terms.
+
+<b>12. Contact Us</b>
+    ● If you have any questions about these Terms, please contact us at
+      info@attmans.in
+  `;
+
+
+
+export const freelancerTerms = `
+   Terms and Conditions for PhD Scholar Research Collaborators/Supply side user
+These Terms and Conditions ("Terms") govern the use of the ATTMANS by PhD
+scholars acting as research collaborators ("Collaborators''). By registering and using the
+ATTMANS, you agree to be bound by these Terms.
+<b>1. Scope of Services</b>
+   ● Collaborators will provide research collaboration services to companies
+     ("Demand side user") on the ATTMANS.
+   ● The specific scope of services will be outlined in each individual project
+     agreement between a Collaborator and a Client.
+<b>2. Project Agreements</b>
+   ● Collaborators and Demand side users will enter into separate project agreements
+     for each collaboration.
+   ● These agreements should detail the specific research project, deliverables,
+     timelines, fees, and intellectual property ownership.
+<b>3. Intellectual Property</b>
+   ● Ownership of intellectual property (IP) created during a collaboration will be
+     determined in the project agreement.
+   ● Common options include:
+   ○ Client owns the IP with Collaborator receiving acknowledgement and
+     potential co-authorship.
+   ○ Collaborator retains ownership but grants the Client a license to use the IP
+     for specific purposes.
+
+   ● Both parties are responsible for adhering to any third-party IP used in the project.
+
+<b>4. Confidentiality</b>
+   ● Collaborators agree to keep confidential any non-public information disclosed by
+     Demand side users during a collaboration.
+   ● Exceptions may include publicly known information or information independently
+     developed by the Collaborator.
+<b>5. Disclaimers and Warranties</b>
+   ● The ATTMANS is provided "as is" and without warranties of any kind.
+   ● Collaborators are responsible for the quality and accuracy of their work.
+   ● The ATTMANS does not guarantee the success of any collaboration or the
+     suitability of any Collaborator.
+<b>6. Termination</b>
+
+   ● These Terms and a Collaborator's access to the ATTMANS may be terminated
+     by either party for any reason with written notice.
+   ● Termination of a project agreement will be governed by the terms of that
+     agreement.
+<b>7. Governing Law and Dispute Resolution</b>
+If a dispute arises between you and ATTMANS, our goal is to resolve the dispute
+quickly and cost-effectively. Accordingly, you and ATTMANS agree to resolve any
+claim or controversy at law or equity that arise out of or relate to this Agreement or
+the Services (each, a "Claim") by referring the Claim to binding arbitration. The
+arbitration shall be conducted in Ahmedabad, Gujarat, India in accordance with the
+terms of the Indian Law Arbitration Rules which are incorporated herein by way of
+reference.
+Subject to arbitration, this Agreement including without limitation any dispute
+relating to the Services, shall be governed by and construed in accordance with
+Indian Laws, without regard to its conflict of law provisions and the Courts of
+Ahmedabad shall have the sole jurisdiction in this regard.
+
+<b>8. Independent Contractor</b>
+   ● Collaborators are independent contractors, not employees of the ATTMANS.
+<b>9. Updates to Terms</b>
+   ● The ATTMANS reserves the right to update these Terms at any time.
+   ● Continued use of the ATTMANS after the update constitutes your agreement to
+     the revised Terms.
+  `;
+
+
+export const projectOwnerTerms = `
+    Terms and Conditions for Companies/Demand side user
+Welcome! These Terms and Conditions ("Terms") govern your access to and use of
+our platform (the "Platform") where you can connect with PhD scholars for research
+collaboration and purchase innovative products from independent innovators
+(collectively, the "Services"). By accessing or using the Platform, you agree to be
+bound by these Terms.
+<b>1. Using the Platform</b>
+   ● Eligibility: You must be a registered business entity to use the Services.
+   ● Permitted Use: You may use the Platform to:
+   o Search for and connect with PhD scholars for research collaboration.
+   o Browse and purchase innovative products offered by independent innovators.
+   ● Prohibited Use: You may not:
+   o Violate any applicable laws or regulations.
+   o Infringe on the intellectual property rights of others.
+   o Engage in any fraudulent or misleading activity.
+   o Use the Platform for any purpose other than its intended use.
+<b>2. Collaboration with PhD Scholars</b>
+   ● Independent Contractors: PhD scholars are independent contractors and not
+   employees of our platform.
+   ● Engagement Terms: All terms of collaboration, including project scope, fees, and
+     intellectual property ownership, must be agreed upon directly between you and the
+     scholar. We recommend using a written agreement for clarity.
+   ● Background Checks: We may or may not conduct background checks on scholars.
+     It is your responsibility to conduct any necessary due diligence before engaging with
+     a scholar.
+<b>3. Purchase of Innovative Products</b>
+   ● Product Descriptions: Product descriptions and pricing are provided by the
+     innovators themselves. We are not responsible for the accuracy of this information.
+   ● Payment Processing: We may use a third-party payment processor to facilitate
+     transactions. You agree to be bound by the terms of service of the payment
+     processor.
+   ● Delivery and Returns: Delivery and return policies are determined by the individual
+     innovator. Please refer to the product listing for details.
+   ● Warranties: Warranties, if any, are provided by the innovator, not our platform.
+<b>4. Intellectual Property</b>
+   ● Platform Content: We own the intellectual property rights to the Platform and its
+     content.
+   ● User Content: You retain ownership of any content you submit to the Platform
+     ("User Content"). You grant us a non-exclusive license to use, reproduce, modify,
+     publish, and distribute your User Content in connection with the Services.
+<b>5. Disclaimers and Limitations of Liability</b>
+   ● We make no warranties, express or implied, about the Services. The platform
+     and all content are provided "as is" and without warranties of any kind.
+
+   ● We will not be liable for any damages arising out of your use of the Services.
+     This includes, but is not limited to, direct, indirect, incidental, consequential, or
+     punitive damages.
+<b>6. Termination</b>
+   ● We may terminate your access to the Platform for any reason, at any time, and
+     without notice.
+<b>7. Governing Law and Dispute Resolution</b>
+If a dispute arises between you and ATTMANS, our goal is to resolve the dispute
+quickly and cost-effectively. Accordingly, you and ATTMANS agree to resolve any
+claim or controversy at law or equity that arise out of or relate to this Agreement
+or the Services (each, a "Claim") by referring the Claim to binding arbitration.
+The arbitration shall be conducted in Ahmedabad, Gujarat, India in accordance
+with the terms of the Indian Law Arbitration Rules which are incorporated herein
+by way of reference.
+Subject to arbitration, this Agreement including without limitation any dispute
+relating to the Services, shall be governed by and construed in accordance with
+Indian Laws, without regard to its conflict of law provisions and the Courts of
+Ahmedabad shall have the sole jurisdiction in this regard.
+
+<b>8. Entire Agreement</b>
+   ● These Terms constitute the entire agreement between you and ATTMANS regarding
+     your use of the Services.
+<b>9. Changes to the Terms</b>
+   ● We may update these Terms from time to time. We may notify you of any changes by
+     posting the new Terms on the Platform. Your continued use of the Platform after the
+     posting of the revised Terms constitutes your acceptance of the revised Terms.
+<b>10. Contact Us</b>
+If you have any questions about these Terms, please contact us at info@attmans.in
+  `;
